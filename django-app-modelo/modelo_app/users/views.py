886225fd37c92832cbe65d9c590fe06846d0c0ee
renzo <renzo.dealba@gmail.com>
2025-02-13 18:58:57 -0600

    maquetado

--- django-app-modelo/modelo_app/users/views.py
+++ django-app-modelo/modelo_app/users/views.py
@@ -4,5 +4,5 @@ from .models import Users
 
 def usersIndex(request):
     # Obtener todos los usuarios sin direcciones
-    users = Users.objects.all().values('id', 'name', 'email')
+    users = Users.objects.all().values('id', 'name', 'email', 'photo')
     return render(request, 'users/index.html', {'users': users})
